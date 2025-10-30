9ce4e2990068f0393a7f14f574ac8253bbbdd79b
using6843@gmail.com <using6843@gmail.com>
2025-10-31 02:58:18 +0900

    mission: 5주차 미션2

--- mission/Chapter05/src/apis/axios.ts
+++ mission/Chapter05/src/apis/axios.ts
@@ -1,17 +1,105 @@
-import axios from "axios";
+import axios, { InternalAxiosRequestConfig } from "axios";
 import { LOCAL_STORAGE_KEY } from "../constants/keys";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+interface CustomInternalAxiosRequestConfig extends InternalAxiosRequestConfig {
+    _retry?: boolean;
+}
+
+let refreshPromise: Promise<string> | null = null;
 
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_SERVER_API_URL,
 });
 
-axiosInstance.interceptors.request.use((config) => {
-    const token = window.localStorage.getItem(LOCAL_STORAGE_KEY.accessToken);
-    if (token) {
-        config.headers = config.headers ?? {};
-        config.headers.Authorization = `Bearer ${token}`;
-    } else if (config.headers?.Authorization) {
-        delete config.headers.Authorization;
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const { getItem } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
+        const accessToken = getItem();
+
+        if (accessToken) {
+            config.headers = config.headers || {};
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest: CustomInternalAxiosRequestConfig = error.config;
+
+        if (
+            error.response &&
+            error.response.status === 401 &&
+            !originalRequest._retry
+        ) {
+            if (originalRequest.url === "/v1/auth/refresh") {
+                const { removeItem: removeAccessToken } = useLocalStorage(
+                    LOCAL_STORAGE_KEY.accessToken
+                );
+
+                const { removeItem: removeRefreshToken } = useLocalStorage(
+                    LOCAL_STORAGE_KEY.refreshToken
+                );
+
+                removeAccessToken();
+                removeRefreshToken();
+                window.location.href = "/login";
+                return Promise.reject(error);
+            }
+
+            originalRequest._retry = true;
+
+            if (!refreshPromise) {
+                refreshPromise = (async () => {
+                    const { getItem: getRefreshToken } = useLocalStorage(
+                        LOCAL_STORAGE_KEY.refreshToken
+                    );
+                    const refreshToken = getRefreshToken();
+
+                    const { data } = await axiosInstance.post("/v1/auth/refresh", {
+                        refresh: refreshToken
+                    })
+
+                    const { setItem: setAccessToken } = useLocalStorage(
+                        LOCAL_STORAGE_KEY.accessToken
+                    );
+
+                    const { setItem: setRefreshToken } = useLocalStorage(
+                        LOCAL_STORAGE_KEY.refreshToken
+                    );
+
+                    setAccessToken(data.data.accessToken);
+                    setRefreshToken(data.data.refreshToken);
+
+                    return data.data.accessToken;
+                })()
+                    .catch((error) => {
+                        const { removeItem: removeAccessToken } = useLocalStorage(
+                            LOCAL_STORAGE_KEY.accessToken
+                        );
+                        const { removeItem: removeRefreshToken } = useLocalStorage(
+                            LOCAL_STORAGE_KEY.refreshToken
+                        );
+
+                        removeAccessToken();
+                        removeRefreshToken();
+                    }).finally(() => {
+                        refreshPromise = null;
+                    });
+            }
+
+            return refreshPromise.then((newAccessToken) => {
+                originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+
+                return axiosInstance.request(originalRequest);
+            });
+        }
+
+        return Promise.reject(error);
     }
-    return config;
-});
+)
